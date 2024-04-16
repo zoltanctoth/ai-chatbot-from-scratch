6c01751fd51fdfe0677257459c84d86081834f6c
Zoltan C. Toth <zoltan@zoltanctoth.com>
2024-04-16 11:55:10 +0200

    initial commit

---
+++ final.py
@@ -0,0 +1,36 @@
+import chainlit as cl
+from ctransformers import AutoModelForCausalLM
+
+llm = AutoModelForCausalLM.from_pretrained(
+    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+)
+
+SYSTEM = """
+You are an AI assistant that follows instruction extremely well. Help as much as you can. Give short answers. Use the context provided."
+"""
+
+
+def get_prompt(instruction, conversation_history=[]):
+    context = ". ".join(conversation_history)
+    prompt = f"### System:\n{SYSTEM}. Context: {context}\n\n### User:\n{instruction}\n\n### Response:\n"
+    print(f"\n----------------------------------\nPROMPT: {prompt}")
+    return prompt
+
+
+history = []
+
+instruction = "What's the weight of an average male?"
+prompt = get_prompt(instruction)
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    prompt = get_prompt(message.content, history)
+    history.append(message.content)
+
+    msg = cl.Message(content="")
+    await msg.send()
+
+    for token in llm(prompt, stream=True):
+        await msg.stream_token(token)
+    await msg.update()
